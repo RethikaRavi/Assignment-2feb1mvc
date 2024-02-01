a5a1f26e7f8a6526d5c7df5d8d0a3886b8577709
deepikarethika <deepikasridhar2001@gmail.com>
2024-02-01 17:54:17 +0530

    changes

---
+++ Assign-2mvc/Controllers/CustomController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Assign_2mvc.Controllers
+{
+    public class CustomController : Controller
+    {
+        // GET: Custom
+        public ActionResult Index()
+        {
+            ViewBag.msg = "These are the Customers";
+            List<string> list = new List<string>
+            {"Robert",
+            "George",
+            "Anabell",
+            "Nitin",
+            "Deepak"
+                    };
+            ViewBag.nameList = list;
+            return View();
+        }
+        public ActionResult RedirectDemo()
+        {
+            TempData["displaymsg"] = "Thanks for visiting us!!!";
+            return View();
+        }
+    }
+}
